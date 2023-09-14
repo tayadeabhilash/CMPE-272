8c8e942f52cbfdb9d52918933931d353a93e4258
Umang Patel <umang26patel@gmail.com>
2023-09-13 21:59:18 -0700

    h/w2

---
+++ Homework 2/TwitterService/src/main/java/com/example/twitterservice/TwitterServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.twitterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    // Umang`s Spring Boot Application
+
+@SpringBootApplication
+public class TwitterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterServiceApplication.class, args);
+    }
+
+}
---
+++ Homework 2/TwitterService/src/test/java/com/example/twitterservice/TwitterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
