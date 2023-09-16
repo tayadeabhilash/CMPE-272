73eea4e3156832f2ac08e8c9e166f1170307e500
Alan Park <28246519+alansdpark@users.noreply.github.com>
2023-09-15 21:33:12 -0700

    test cases for tweet and delete

--- Homework 2/TwitterService/src/test/java/com/example/twitterservice/TwitterServiceApplicationTests.java
+++ Homework 2/TwitterService/src/test/java/com/example/twitterservice/TwitterServiceApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.twitterservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TwitterServiceApplicationTests {
 
+    @Autowired
+    private TwitterController controller;
+
     @Test
     void contextLoads() {
+        assertNotNull(controller);
     }
 
+    @Test
+    void happy_POSTTweet() {
+        try {
+            assertEquals(controller.tweet("Testing"), "200");
+        }
+        catch(Exception e) {}
+    }
+
+    @Test
+    void happy_DELETETweet() {
+        try {
+            assertEquals(controller.delete(0), "200");
+        }
+        catch(Exception e) {}
+    }
 }
