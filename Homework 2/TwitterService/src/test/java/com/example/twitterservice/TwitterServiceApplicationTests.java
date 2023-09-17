461ecb7f4d296c459983b8ce8e7bba976835b2ba
Alan Park <28246519+alansdpark@users.noreply.github.com>
2023-09-16 22:29:59 -0700

    Test cases for tweet and delete (#3)
    * test cases for tweet and delete
    
    * Added name
    
    * Got tests to work

--- Homework 2/TwitterService/src/test/java/com/example/twitterservice/TwitterServiceApplicationTests.java
+++ Homework 2/TwitterService/src/test/java/com/example/twitterservice/TwitterServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.twitterservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * Alan's Test Cases
+ */
 @SpringBootTest
 class TwitterServiceApplicationTests {
 
+    @Autowired
+    private TwitterController controller;
+
     @Test
     void contextLoads() {
+        assertNotNull(controller);
     }
 
+    @Test
+    void happy_POSTTweet() {
+        try {
+            assertNotNull(controller.tweet("Testing API POST"));
+        }
+        catch(Exception e) {
+            throw new RuntimeException("Failed POST test");
+        }
+    }
+
+    @Test
+    void happy_DELETETweet() {
+        try {
+            assertNotNull(controller.delete(0));
+        }
+        catch(Exception e) {
+            throw new RuntimeException("Failed DELETE test");
+        }
+    }
 }
